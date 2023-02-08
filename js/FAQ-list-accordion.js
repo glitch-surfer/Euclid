f762317f76366054eaefae7076ce3b8bf1eea26b
vladislav-garaev <marker661@gmail.com>
2023-02-08 21:41:28 +0300

    final

--- js/FAQ-list-accordion.js
+++ js/FAQ-list-accordion.js
@@ -1,8 +1,8 @@
 window.addEventListener('DOMContentLoaded', function () {
     document.querySelectorAll('.FAQ-list__btn').forEach(function (el) {
         el.addEventListener('click', function() {
+            el.classList.toggle('icon-rotate')
             let content = el.nextElementSibling
-            
             content.classList.toggle('FAQ-list__accordion-is-active')
             
     })
